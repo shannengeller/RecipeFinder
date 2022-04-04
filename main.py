6d2760464e9dae1403974fce3007c48b65748b5f
Shannen Geller <shannengeller7@gmail.com>
2022-04-04 09:13:58 -0400

    commit

--- main.py
+++ main.py
@@ -2,12 +2,29 @@ from distutils.version import Version
 from setuptools import setup, find_packages
 from apiKey import key
 
-#read api key
-
-
 #example
 import spoonacular as sp
 api = sp.API(key)
+
+# Print beginning stuff
+print("Welcome to the Recipe Finder!")
+print()
+print("If you would like to see a full recipe, please enter 'full'")
+print("If you would like to see a list of ingredients, please enter 'list'")
+print("If you would like to see a list of recipes, please enter 'recipes'")
+print("If you would like to see a list of ingredients, please enter 'ingredients'")
+print()
+
+# Get user input
+user_input = input("Please enter 'full', 'list', 'recipes', or 'ingredients': ")
+
+# If user input is 'full'
+if user_input == "full":
+    print("Please enter the name of the recipe you would like to see: ")
+    recipe_name = input()
+    print()
+    
+    
 
 # Parse an ingredient
 response = api.parse_ingredients("3.5 cups King Arthur flour", servings=1)
